115fdef5b79a22907f080e56dcb6abd3cd68c2bf
mtdx <ms2asdk@gmail.com>
2018-11-21 22:43:55 +0200

    items

--- src/main/webapp/app/shared/layout/header/menus/entities.tsx
+++ src/main/webapp/app/shared/layout/header/menus/entities.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import { DropdownItem } from 'reactstrap';
+import { DropdownItem, NavItem, NavLink } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { NavLink as Link } from 'react-router-dom';
 import { NavDropdown } from '../header-components';
 
 export const EntitiesMenu = props => (
-  // tslint:disable-next-line:jsx-self-close
-  <NavDropdown icon="th-list" name="Entities" id="entity-menu">
-    <DropdownItem tag={Link} to="/entity/vgoitem">
-      <FontAwesomeIcon icon="asterisk" fixedWidth />
-      &nbsp;Vgoitem
-    </DropdownItem>
-    {/* jhipster-needle-add-entity-to-menu - JHipster will add entities to the menu here */}
-  </NavDropdown>
+  <>
+    <NavItem>
+      <NavLink tag={Link} to="/entity/vgoitem">
+        <FontAwesomeIcon icon="list" />
+        <span>VGO</span>
+      </NavLink>
+    </NavItem>
+    <NavItem>
+      <NavLink tag={Link} to="/">
+        <FontAwesomeIcon icon="list" />
+        <span>CSGO</span>
+      </NavLink>
+    </NavItem>
+  </>
 );
