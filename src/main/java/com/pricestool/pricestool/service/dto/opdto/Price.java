0a6834b54bc6ccdcb088dffe911f189394bed015
mtdx <ms2asdk@gmail.com>
2018-11-22 20:20:51 +0200

    fix naming

--- src/main/java/com/pricestool/pricestool/service/dto/opdto/Price.java
+++ src/main/java/com/pricestool/pricestool/service/dto/opdto/Price.java
@@ -4,9 +4,9 @@ public class Price {
     private int op_7_day;
     private int op_30_day;
 
-    public int get_op_7_day() { return op_7_Day; }
+    public int get_op_7_day() { return op_7_day; }
     public void set_op_7_day(int value) { this.op_7_day = value; }
 
-    public int get_op_30_day() { return op_30_Day; }
+    public int get_op_30_day() { return op_30_day; }
     public void set_op_30_day(int value) { this.op_30_day = value; }
 }
